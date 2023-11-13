c5f59b650413a8a4477201322840af8c446dbb52
Server <nd7433@gmail.com>
2023-11-13 22:15:26 +0700

    cập nhật thêm ảnh từ máy local

--- models/Post.js
+++ models/Post.js
@@ -22,6 +22,11 @@ const postSchema = new mongoose.Schema({
         type: String,
     },
 
+    image:{
+        type: String,
+        required: true,
+    },
+
     likeCount: {
         type: Number,
         default: 0,
