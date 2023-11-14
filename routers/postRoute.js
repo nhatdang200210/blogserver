74086dab80e11cfb404bb0995b57176ef704454e
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-11-15 04:10:19 +0700

    fix error params route

--- routers/postRoute.js
+++ routers/postRoute.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const {getPosts, createPost, updatePost, deletePost} = require( '../controllers/postController');
 
-const {verifyToken} = require('../middlewares/verifyToken.js')
+// const {verifyToken} = require('../middlewares/verifyToken.js')
 
 const Router = express.Router();
 
 Router.route('/').get(getPosts).post(createPost);
-Router.route('/:posId').put(verifyToken, updatePost).delete(verifyToken, deletePost);
+Router.route('/:postId').put(updatePost).delete(deletePost);
 
 module.exports = Router;
